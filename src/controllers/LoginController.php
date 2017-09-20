d6e24c094296a683ccfd53fa1607a18d35fc459b
TJ Draper <tj@buzzingpixel.com>
2017-09-20 15:54:12 -0500

    Re-implement partial login controller

--- src/controllers/LoginController.php
+++ src/controllers/LoginController.php
@@ -2,6 +2,7 @@
 
 namespace topshelfcraft\legacylogin\controllers;
 
+use Craft;
 use craft\web\Controller;
 
 /**
@@ -19,10 +20,48 @@ class LoginController extends Controller
     /**
      * Handles log-in via POST request
      * @return null
+     * @throws \Exception
      */
     public function actionLogin()
     {
-        var_dump('here');
+        // Bail if this isn't a POST request
+        $this->requirePostRequest();
+
+        // If user is already logged in skip to processing as successful
+        if (Craft::$app->getUser()->identity !== null) {
+            $this->handleSuccessfulLogin();
+        }
+
+        // A little house-cleaning for expired, pending users.
+        // (Same as in the UsersController, natch.)
+        Craft::$app->getUsers()->purgeExpiredPendingUsers();
+
+        // Okay, let's go
+
+        // Get the request service
+        $requestService = Craft::$app->getRequest();
+
+        $loginName = $requestService->post('loginName');
+        $password = $requestService->post('password');
+        $rememberMe = (bool) $requestService->post('rememberMe');
+
+        // TODO: implement the login service method
+        var_dump('hey, we should do some stuff here');
+        die;
+    }
+
+    /**
+     * Handles the post-login process
+     * @param string $loginType legacy|craft
+     * @param bool $setNotice Whether a flash notice should be set if not AJAX
+     */
+    private function handleSuccessfulLogin(
+        string $loginType = '',
+        bool $setNotice = true
+    ) {
+        // TODO: handle successful login
+        var_dump($loginType);
+        var_dump($setNotice);
         die;
     }
 }
